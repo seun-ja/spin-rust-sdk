3e8cf5f81323e4bcce6038fe53236f304e026720
Aminu Oluwaseun Joshua <seun.aminujoshua@gmail.com>
2025-09-24 14:26:44 +0100

    added increment
    Signed-off-by: Aminu Oluwaseun Joshua <seun.aminujoshua@gmail.com>

--- src/key_value_atomics.rs
+++ src/key_value_atomics.rs
@@ -1,9 +1,13 @@
+use std::sync::atomic::Ordering;
+
 use crate::wit::wasi::keyvalue::store::KeyResponse;
 
 use super::wit::wasi::keyvalue;
 
 pub use keyvalue::atomics::Bucket;
 pub use keyvalue::atomics::Cas;
+
+pub use keyvalue::atomics;
 
 pub use keyvalue::atomics::CasError;
 pub use keyvalue::atomics::Error as AtomicsError;
@@ -15,24 +19,46 @@ impl keyvalue::atomics::Bucket {
     }
 }
 
+/// Increments the value of a key in the bucket.
+pub fn atomics_increment(bucket: &Bucket, key: &str, value: i64) -> Result<i64, anyhow::Error> {
+    Ok(atomics::increment(bucket, key, value)?)
+}
+
 impl keyvalue::atomics::Bucket {
     /// Retrieves a key-value pair from the bucket.
-    pub fn atomic_get(&self, key: &str) -> Result<Option<Vec<u8>>, anyhow::Error> {
+    pub fn atomic_get(
+        &self,
+        key: &str,
+        _ordering: Ordering, // Ideally an Ordering::Acquire,
+    ) -> Result<Option<Vec<u8>>, anyhow::Error> {
         Ok(self.get(key)?)
     }
 
     /// Sets a key-value pair in the bucket.
-    pub fn atomic_set(&self, key: &str, value: &[u8]) -> Result<(), anyhow::Error> {
+    pub fn atomic_set(
+        &self,
+        key: &str,
+        value: &[u8],
+        _ordering: Ordering, // Ideally an Ordering::Release,
+    ) -> Result<(), anyhow::Error> {
         Ok(self.set(key, value)?)
     }
 
     /// Deletes a key-value pair from the bucket.
-    pub fn atomic_delete(&self, key: &str) -> Result<(), anyhow::Error> {
+    pub fn atomic_delete(
+        &self,
+        key: &str,
+        _ordering: Ordering, // Ideally an Ordering::Release,
+    ) -> Result<(), anyhow::Error> {
         Ok(self.delete(key)?)
     }
 
     /// Checks if a key-value pair exists in the bucket.
-    pub fn atomic_exists(&self, key: &str) -> Result<bool, anyhow::Error> {
+    pub fn atomic_exists(
+        &self,
+        key: &str,
+        _ordering: Ordering, // Ideally an Ordering::Relaxed,
+    ) -> Result<bool, anyhow::Error> {
         Ok(self.exists(key)?)
     }
 
@@ -58,3 +84,5 @@ impl keyvalue::atomics::Cas {
         Ok(self.current()?)
     }
 }
+
+mod test {}
